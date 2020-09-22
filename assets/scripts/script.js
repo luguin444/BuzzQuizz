fb3a866f2ae30968d1e6dd2a855c08ad6df3bfb7
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2020-09-22 00:15:01 -0300

    Registro do usuário no servidor na etapa de login

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,27 @@
+var userEmail;    //luguin
+var userPassword; //8998
+var token;
 
-
-function validateData () {
+function validateDataWithServer () {
     
-    checkIfFIeldsAreBlanck ();
+    if (checkIfFIeldsAreBlanck ()) {
+
+        data = {email: userEmail, password: userPassword};
+        
+        var requestPostUser = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users",data);
+        requestPostUser.then(teste);
+        requestPostUser.catch(RegisterUserError);
+    }
+}
+
+
+function teste (response) {
+    token = response.data["token"];
+    console.log(token);
+}
+
+function RegisterUserError() {
+    console.log("deu ruim");
 }
 
 
@@ -14,6 +33,13 @@ function checkIfFIeldsAreBlanck () {
     var EmptyLogin = inputLogin.value === "";
     var EmptyPassword = inputPassword.value === "";
 
-    if (EmptyLogin || EmptyPassword) 
+    if (EmptyLogin || EmptyPassword)  {
         alert("Por favor, preencha os campos de email e senha.");
+        return false;
+    }
+
+    userEmail = inputLogin.value;
+    userPassword = inputPassword.value;
+    return true;
+      
 }
